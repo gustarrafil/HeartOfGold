3696c46a6ae591b191f0a793e227a30af2c08843
Gustavo Tarrafil Sousa Pereira <gustavotarrafil@gmail.com>
2021-02-11 19:46:07 -0300

    update projeto 1 python

--- estudo_python/projeto_1/main.py
+++ estudo_python/projeto_1/main.py
@@ -19,31 +19,42 @@ def add_movie():
         'year': year
     })
 
+def print_movie(movie):
+    print(f"\nMovie: {movie['title']}\nDirected by: {movie['director']}\nReleased on: {movie['year']}")
+
+
 def list_movies():
-    for item in movies:
-        print(f"\nMovie: {item['title']}\nDirected by: {item['director']}\nReleased on: {item['year']}")
+    for movie in movies:
+        print_movie(movie)
 
 
 def find_movie():
-    title = input("Enter the movie title: ")
-    for item in movies:
-        if item['title'] == title:
-            print(f"\nMovie: {item['title']}\nDirected by: {item['director']}\nReleased on: {item['year']}")
+    title = input("Enter the movie title you're looking for: ")
+    for movie in movies:
+        if movie['title'] == title:
+            print_movie(movie)
+
 
 ###################################################################################
 
 MENU_PROMPT = "\nEnter 'a' to add a movie, 'l' to see your movies, 'f' to find a movie by title, or 'q' to quit: "
 
-selection = input(MENU_PROMPT)
+user_options = {
+    'a': add_movie,
+    'l': list_movies,
+    'f': find_movie,
+}
 
-while selection != 'q':
-    if selection == 'a':
-        add_movie()
-    elif selection == 'l':
-        list_movies()
-    elif selection == 'f':
-        find_movie()
-    else:
-        print("Unknown command. Please try again.")
 
+def menu():
     selection = input(MENU_PROMPT)
+    while selection != 'q':
+        if selection in user_options:
+            selected_function = user_options[selection]
+            selected_function()
+        else:
+            print("Unknown command. Please try again.")
+
+        selection = input(MENU_PROMPT)
+
+menu()
