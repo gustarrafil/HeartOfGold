0ea405c2cc509c7edf027cbe2581826f01853b36
Gustavo Tarrafil Sousa Pereira <gustavotarrafil@gmail.com>
2023-03-25 16:29:19 -0300

    feat(baltaio): objetos complexos

--- estudo_angular/todo/src/app/app.component.ts
+++ estudo_angular/todo/src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Todo } from 'src/models/todo.model';
 
 @Component({
   selector: 'app-root',
@@ -6,16 +7,22 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  public todos: any[] = [];
+  public todos: Todo[] = [];
   public title: string = 'minhas tarefas';
 
   /**
    *
    */
   constructor() {
-    this.todos.push('passear com cachorro');
-    this.todos.push('ir ao supermercado');
-    this.todos.push('cortar o cabelo');
+    this.todos.push(
+      new Todo(1, 'passear com cachorro', false)
+    );
+    this.todos.push(
+      new Todo(2, 'ir ao supermercado', false)
+    );
+    this.todos.push(
+      new Todo(3, 'cortar o cabelo', true)
+    );
   }
 
   alterarTexto() {
