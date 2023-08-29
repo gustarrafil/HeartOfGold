e04fee5a08e6d62e2ebde5628bfe21555919adcc
Gustavo Tarrafil Sousa Pereira <gustavotarrafil@gmail.com>
2023-08-28 21:22:04 -0300

    feat(cypress): testing tests

--- estudo_cypress/cypress/e2e/my_first_test.cy.js
+++ estudo_cypress/cypress/e2e/my_first_test.cy.js
@@ -1,8 +1,26 @@
 /// <reference types="cypress" />
 
-it('google test', () => {
-    cy.visit('https://google.com')
+it('visiting university website', () => {
+    cy.visit('https://www.ft.unicamp.br/')
 
-    cy.get('#APjFqb').type('automation step by step{enter}')
+    cy.get('#menu-1137-1 > .sf-depth-1').click()
+
+    cy.get('.menu-mlid-1214 > a').click()
+
+    cy.get('.menu-mlid-1219 > a').click()
+
+    cy.get('#edit-search-block-form--2').click().type('lala')
+
+    cy.get('#edit-submit--2').click()
+
+})
+
+it('almost login twitter not password', () => {
+    cy.visit('https://twitter.com/')
+
+    cy.get('[data-testid="loginButton"] > .r-1awozwy').click()
+
+    cy.get('.r-1roi411 > :nth-child(1) > .r-16y2uox').type('gustarrafil')
+
 
 })
