37347945f1e9214a7dc1246880a46cf3cc4d34ac
Gustavo Tarrafil Sousa Pereira <gustavotarrafil@gmail.com>
2023-10-23 14:43:48 -0300

    feat(react): new page with router

--- estudo_react/mynotes/src/App.js
+++ estudo_react/mynotes/src/App.js
@@ -1,13 +1,25 @@
+import {
+  BrowserRouter as Router,
+  Route,
+  Routes,
+} from "react-router-dom";
+
 import './App.css';
 import Header from './components/Header'
 import NotesListPage from './pages/NotesListPage';
+import NotePage from './pages/NotePage'
 
 function App() {
   return (
-    <div className="App">
-      <Header />
-      <NotesListPage />
-    </div>
+    <Router>
+      <div className="App">
+        <Header />
+        <Routes>
+          <Route path="/" exact Component={NotesListPage} />
+          <Route path="/note" Component={NotePage} />
+        </Routes>
+      </div>
+    </Router>
   );
 }
 
