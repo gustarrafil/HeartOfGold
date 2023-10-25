1c0e756024f4494592a6c13e36013aabc31277a7
Gustavo Tarrafil Sousa Pereira <gustavotarrafil@gmail.com>
2023-10-25 10:49:18 -0300

    feat(react): adding general styles

--- estudo_react/mynotes/src/components/Header.js
+++ estudo_react/mynotes/src/components/Header.js
@@ -1,7 +1,7 @@
 const Header = () => {
     return (
-        <div>
-            <h1>my header</h1>
+        <div className="app-header">
+            <h1>Notes List</h1>
         </div>
     )
 }
--- estudo_react/mynotes/src/pages/NotePage.js
+++ estudo_react/mynotes/src/pages/NotePage.js
@@ -1,6 +1,9 @@
 import React from 'react'
 import { useParams } from 'react-router-dom'
 import notes from '../assets/data'
+import {Link} from 'react-router-dom'
+import {ReactComponent as ArrowLeft} from '../assets/arrow-left.svg'
+
 
 const NotePage = () => {
     let noteId = useParams().id
@@ -8,8 +11,16 @@ const NotePage = () => {
     let note = notes.find(note => note.id === Number(noteId))
 
   return (
-    <div>
-        <p>{note?.body}</p>
+    <div className='note'>
+        <div className='note-header'>
+            <h3>
+                <Link to="/" >
+                    <ArrowLeft />
+                </Link>
+            </h3>
+        </div>
+        <textarea value={note?.body}>
+        </textarea>
     </div>
   )
 }
