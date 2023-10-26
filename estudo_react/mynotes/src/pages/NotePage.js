db92c2d4b25b70d83819a48ba49f801e7e005157
Gustavo Tarrafil Sousa Pereira <gustavotarrafil@gmail.com>
2023-10-26 10:24:44 -0300

    feat(react): making API calls

--- estudo_react/mynotes/src/pages/NotePage.js
+++ estudo_react/mynotes/src/pages/NotePage.js
@@ -1,6 +1,6 @@
-import React from 'react'
+import React, {useEffect, useState} from 'react'
 import { useParams } from 'react-router-dom'
-import notes from '../assets/data'
+// import notes from '../assets/data'
 import {Link} from 'react-router-dom'
 import {ReactComponent as ArrowLeft} from '../assets/arrow-left.svg'
 
@@ -8,7 +8,18 @@ import {ReactComponent as ArrowLeft} from '../assets/arrow-left.svg'
 const NotePage = () => {
     let noteId = useParams().id
 
-    let note = notes.find(note => note.id === Number(noteId))
+    // let note = notes.find(note => note.id === Number(noteId))
+    let [note, setNote] = useState(null)
+
+    useEffect(() => {
+        getNote()
+    }, [noteId])
+
+    let getNote = async () => {
+        let response = await fetch(`http://localhost:8000/notes/${noteId}`)
+        let data = await response.json()
+        setNote(data)
+    }
 
   return (
     <div className='note'>
