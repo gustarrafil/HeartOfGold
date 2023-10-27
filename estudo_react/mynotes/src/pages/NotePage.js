309a4172df537c05e2ca9844191067daa622ff41
Gustavo Tarrafil Sousa Pereira <gustavotarrafil@gmail.com>
2023-10-27 09:39:29 -0300

    feat(react): supposed addind but not working smooth

--- estudo_react/mynotes/src/pages/NotePage.js
+++ estudo_react/mynotes/src/pages/NotePage.js
@@ -16,9 +16,20 @@ const NotePage = ({history}) => {
     }, [noteId])
 
     let getNote = async () => {
+        if (noteId === 'new') return
         let response = await fetch(`http://localhost:8000/notes/${noteId}`)
         let data = await response.json()
         setNote(data)
+    }
+
+    let createNote = async () => {
+        await fetch(`http://localhost:8000/notes/`, {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify({...note, 'updated': new Date()})
+        })
     }
 
     let updateNote = async () => {
@@ -48,8 +59,10 @@ const NotePage = ({history}) => {
 
         if (noteId !== 'new' && !note.body) {
             deleteNote()
-        } else if (noteId === 'new') {
+        } else if (noteId !== 'new') {
             updateNote()
+        } else if (noteId === 'new' && note !== null) {
+            createNote()
         }
 
         updateNote()
@@ -67,7 +80,12 @@ const NotePage = ({history}) => {
                 </Link>
             </h3>
 
-            <button onClick={deleteNote}>Delete</button>
+            {noteId !== 'new' ? (
+                <button onClick={deleteNote}>Delete</button>
+            ): (
+                <button onClick={handleSubmit}>Done</button>
+            )}
+
         </div>
         <textarea onChange={(e) => {setNote({...note, 'body': e.target.value})}} value={note?.body}>
         </textarea>
--- estudo_react/mynotes/src/pages/NotesListPage.js
+++ estudo_react/mynotes/src/pages/NotesListPage.js
@@ -1,6 +1,7 @@
 import React, {useEffect, useState} from 'react'
 // import notes from '../assets/data'
 import ListItem from '../components/ListItem'
+import AddButton from '../components/AddButton'
 
 const NotesListPage = () => {
 
@@ -28,6 +29,7 @@ const NotesListPage = () => {
                 <ListItem key={index} note={note} />
             ))}
       </div>
+      <AddButton />
     </div>
   )
 }
