898bcecab1513355be05ddc714e61673df763c46
Gustavo Tarrafil Sousa Pereira <gustavotarrafil@gmail.com>
2023-10-30 16:50:59 -0300

    feat(angular): event handling

--- estudo_angular/teamapp/src/app/app.component.ts
+++ estudo_angular/teamapp/src/app/app.component.ts
@@ -8,4 +8,14 @@ import { Component } from '@angular/core';
 export class AppComponent {
   newMemberName = "";
   members: string[] = [];
+
+  onInput(member: string) {
+    this.newMemberName = member
+    console.log(this.newMemberName)
+  }
+
+  addMember() {
+    this.members.push(this.newMemberName)
+    console.log(this.members)
+  }
 }
