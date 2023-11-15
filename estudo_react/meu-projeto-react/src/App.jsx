9b59ab224a67052fd0342b184356d128af3f6e8d
Gustavo Tarrafil Sousa Pereira <gustavotarrafil@gmail.com>
2023-11-15 13:46:09 -0300

    feat(react): componentes e props

--- estudo_react/meu-projeto-react/src/App.jsx
+++ estudo_react/meu-projeto-react/src/App.jsx
@@ -1,7 +1,25 @@
+import MeuComponente from "./components/MeuComponente"
+
 function App() {
   return (
+    <div>
       <h1>meu projeto</h1>
+      <MeuComponente />
+      <MeuBotao conteudo="me clique" />
+      <MeuBotao conteudo="depois aqui" />
+      <MeuBotao conteudo="e por fim aqui" idade={2} />
+    </div>
   )
 }
+
+function MeuBotao(props) {
+  console.log(props.conteudo)
+  console.log(props.idade)
+  return (
+    <button>{ props.conteudo }</button>
+  )
+}
+
+
 
 export default App
